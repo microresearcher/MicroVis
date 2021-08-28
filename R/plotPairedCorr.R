79588286c43108a6c76e8911eb4335ce090745eb
Soham A <>
2021-08-28 12:49:03 -0500

    Improved plotPairedCorr to plot all calculable correlations and color them according to R value and statistical significance

--- R/plotPairedCorr.R
+++ R/plotPairedCorr.R
@@ -8,14 +8,24 @@
 #'     compare
 #' @param fts Vector of features to correlate between the two sets
 #' @param invert Switch the x and y axes?
+#' @param rank Taxonomic rank at which to perform the paired correlation. Defaults
+#'     to the second highest rank in the dataset
+#' @param alpha Significance threshold. Defaults to 0.05
+#' @param padj Adjust the p-values for multiple testing? Defaults to TRUE
 #'
 #' @return Scatter plot with trendline
 #' @export
 #'
-plotPairedCor <- function(dataset=NULL, ids, compare, fts=NULL, invert=F) {
+plotPairedCor <- function(dataset=NULL, ids, compare, fts=NULL, rank=NULL,
+                          invert=F,
+                          rthresh=0.7, alpha=0.05, padj=T) {
   if(is.null(dataset)) dataset <- get('active_dataset',envir = mvEnv)
 
-  data <- mvmelt(dataset)
+  # If no valid rank was specified, default to the second highest rank
+  rank <- rank[rank %in% getRanks(dataset)]
+  if(!length(rank)) rank <- getRanks(dataset)[2]
+
+  data <- mvmelt(dataset, rank = rank)
   data$Other <- NULL
   paired_samples <- getSamples(dataset, id_cols = ids, complete = compare)$sample
 
@@ -36,31 +46,44 @@ plotPairedCor <- function(dataset=NULL, ids, compare, fts=NULL, invert=F) {
   data.paired <- data.paired[data.paired[[compare]] %in% groups,]
   ids <- ids[ids %in% colnames(data.paired)]
 
+  cor_rvals <- c()
   cor_pvals <- c()
   for(ft in fts) {
     pivoted <- data.paired[c(ids,compare,ft)] %>% pivot_wider(id_cols=ids,
                                                               names_from=compare,
                                                               values_from=ft)
-    cor_pvals[[ft]] <- rcorr(as.matrix(pivoted[groups]))$P[1,2]
+    corvals <- rcorr(as.matrix(pivoted[groups]))
+    cor_rvals[[ft]] <- corvals$r[1,2]
+    cor_pvals[[ft]] <- corvals$P[1,2]
   }
-  cor_pvals <- p.adjust(cor_pvals, method = 'BH')
-  cor_pvals[is.na(cor_pvals)] <- 1
+  if(padj) cor_pvals <- p.adjust(cor_pvals, method = 'BH')
+
+  cor_rvals[is.na(cor_rvals)] <- 0
+  cor_pvals[is.na(cor_pvals)] <- 1.1
 
   p <- list()
-  for(ft in fts[cor_pvals<=0.05]) {
+  for(ft in fts[cor_pvals<=1]) {
     pivoted <- data.paired[c(ids,compare,ft)] %>% pivot_wider(id_cols=ids,
                                                               names_from=compare,
                                                               values_from=ft)
-    p[[ft]] <- ggscatter(pivoted, groups[1], groups[2], color='blue', size=4,
-                         add='reg.line', add.params=list(size=1.2), conf.int=T,
-                         title=ft)+
-      stat_cor(size=7,show.legend=F)+
-      theme(plot.title = element_text(size=25, hjust=0.5),
-            axis.title = element_text(size=25),
-            axis.text = element_text(size=20),
-            legend.title = element_blank(),
-            legend.key.size = unit(1,'cm'),
-            legend.text = element_text(size=20))
+    if(cor_rvals[match(ft,fts)] >= rthresh & cor_pvals[match(ft,fts)] <= alpha) {
+      ptemp <- ggscatter(pivoted, groups[1], groups[2], color='#00c700', size=4,
+                           add='reg.line', add.params=list(size=1.2), conf.int=T,
+                           title=ft)
+    } else {
+      ptemp <- ggscatter(pivoted, groups[1], groups[2], color='red', size=4, shape=18,
+                           add='reg.line', add.params=list(size=1.2), conf.int=T,
+                           title=ft)
+    }
+
+    p[[ft]] <- ptemp+
+        stat_cor(size=7,show.legend=F)+
+        theme(plot.title = element_text(size=25, hjust=0.5),
+              axis.title = element_text(size=25),
+              axis.text = element_text(size=20),
+              legend.title = element_blank(),
+              legend.key.size = unit(1,'cm'),
+              legend.text = element_text(size=20))
 
     show(p[[ft]])
 
@@ -71,12 +94,21 @@ plotPairedCor <- function(dataset=NULL, ids, compare, fts=NULL, invert=F) {
     else saveFig <- F
 
     if(saveFig) {
-      save_directory <- saveResults(save_path,
+      save_directory <- saveResults(dataset$results_path,
                                     foldername='Paired Correlations',
                                     filename=ft,
                                     forcesave=T,
                                     verbose=F,
                                     width=8,height=6)
     }
+  }
+
+  if(exists('save_directory')) {
+    dir.create(file.path(save_directory,'Statistics'),showWarnings = FALSE)
+
+    write.csv(cor_pvals,
+              file=file.path(save_directory,'Statistics','Statistics.csv'))
+
+    cat('Figures and any associated statistics saved to:\n ',save_directory)
   }
 }
